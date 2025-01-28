ecdb23f2407456ceaad75a3f2ac3a14e6edcf17d
PwetmaluS21 <bautista.argelinaryle@ue.edu.ph>
2025-01-28 02:10:42 +0000

    bahay kubo amp

--- triangle.cpp
+++ triangle.cpp
@@ -7,10 +7,10 @@ int main ()
 
     for (int i = 1; i <= height; ++i) {
         for (int j = 1; j <= height - 1; ++j) {
-            cout << " ";
+            cout << "□";
         }
         for (int j = 1; j <= 2 * 1 - 1; ++j) {
-            cout << "*";
+            cout << "■";
         }
         cout << endl;
     }
