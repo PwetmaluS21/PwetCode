5c94a775f365ce6c1f60784a269eec43a2f26df4
PwetmaluS21 <bautista.argelinaryle@ue.edu.ph>
2025-01-28 02:19:43 +0000

    added roof

--- triangle.cpp
+++ triangle.cpp
@@ -6,10 +6,10 @@ int main ()
     int height = 5;
 
     for (int i = 1; i <= height; ++i) {
-        for (int j = 1; j <= height - 1; ++j) {
-            cout << "□";
+        for (int j = 1; j <= height - i; ++j) {
+            cout << " ";
         }
-        for (int j = 1; j <= 2 * 1 - 1; ++j) {
+        for (int j = 1; j <= 2 * i - 1; ++j) {
             cout << "■";
         }
         cout << endl;
