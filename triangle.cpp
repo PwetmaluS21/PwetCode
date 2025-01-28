5226f76ef3518390cd048e6b15e56db0e069cbeb
PwetmaluS21 <bautista.argelinaryle@ue.edu.ph>
2025-01-28 02:07:54 +0000

    added another file

---
+++ triangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std
+
+int main ()
+{
+    int height = 5;
+
+    for (int i = 1; i <= height; ++i) {
+        for (int j = 1; j <= height - 1; ++j) {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * 1 - 1; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
