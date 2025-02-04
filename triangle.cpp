0cc58855174eca17c5706b5fd92a9b5ef09f81b9
PwetmaluS21 <bautista.argelinaryle@ue.edu.ph>
2025-02-04 01:27:34 +0000

    Bahay ko

--- triangle.cpp
+++ triangle.cpp
@@ -7,17 +7,23 @@ int main ()
 
     for (int i = 1; i <= height; ++i) {
         for (int j = 1; j <= height - i; ++j) {
-            cout << " ";
+            cout << "  "; //space ng bubong
         }
         for (int j = 1; j <= 2 * i - 1; ++j) {
-            cout << "■"; //Ito po yung bubong ng bahay
+            cout << "+ "; //Ito po yung bubong ng bahay
         }
         cout << endl;
     }
 
     for (int i = 1; i <= height; ++i) {
-        for (int j = 1; j <= height - 1; ++j) {
-            cout << "□□"; //Ito po yung bahay yehey
+        for (int j = 1; j <= height + 2; ++j) {
+            if (j<=4) {
+                cout << "□ □ "; //Ito po yung bahay yehey
+            }
+            else if (j>=5) {
+                cout << "— — "; //garage dito
+            }
+           
         }
         cout << endl;
     }
