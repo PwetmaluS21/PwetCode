e8b07424bb4e995ecc1d0feef6ab48101587e33b
PwetmaluS21 <bautista.argelinaryle@ue.edu.ph>
2025-02-04 00:04:25 +0000

    may comment na

--- triangle.cpp
+++ triangle.cpp
@@ -10,14 +10,14 @@ int main ()
             cout << " ";
         }
         for (int j = 1; j <= 2 * i - 1; ++j) {
-            cout << "■";
+            cout << "■"; //Ito po yung bubong ng bahay
         }
         cout << endl;
     }
 
     for (int i = 1; i <= height; ++i) {
         for (int j = 1; j <= height - 1; ++j) {
-            cout << "□□";
+            cout << "□□"; //Ito po yung bahay yehey
         }
         cout << endl;
     }
