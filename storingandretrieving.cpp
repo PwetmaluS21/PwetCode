97c6622d39df9dc05a4fbd44a0d1bbbe99e08a4f
PwetmaluS21 <bautista.argelinaryle@ue.edu.ph>
2025-02-13 12:14:30 +0000

    p['ikp[

--- storingandretrieving.cpp
+++ storingandretrieving.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
 using namespace std;
-
-int main()
+main()
 {
-    int a[10];
-    int ctr;
-    for(ctr=0;ctr<=9;ctr++)
-    {
-        cout<<"Enter any Number= ";
-        cin>>a[ctr];
-        }
-        cout<<"OUTPUT IN REVERSE ORDER"<<endl;
-        for(ctr=9;ctr>=0;ctr--)
-        cout<<a[ctr]<<endl;
-        
-        return 0;
-        }
+int a[15];
+int ctr;
+for(ctr=0;ctr<=14;ctr++)
+{
+    cout<<"Enter any Number = ";
+    cin>>a[ctr];
+}
+cout<<“LIST OF EVEN NUMBERS”<<endl;
+for(ctr=0;ctr<=14;ctr++)
+	if(a[ctr] % 2 == 0)
+    		cout<<a[ctr]<<endl;
+return 0;
+}
